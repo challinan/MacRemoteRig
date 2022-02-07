a1ba157a52699ca92173f2cc35f60885c9a47365
challinan <challinan@gmail.com>
2022-02-07 15:17:20 -0500

    Minor fixes after adding Windows 10 support

--- gstreamerlistener.cpp
+++ gstreamerlistener.cpp
@@ -30,9 +30,9 @@ void GstreamerListener::run() {
                                 " encoding-name=(string)L16, encoding-params=(string)1, channels=(int)1, channel-positions=(int)1, payload=(int)96 "
                                 " ! rtpjitterbuffer mode=synced ! rtpL16depay ! audioconvert ! directsoundsink sync=true", &gst_error);
 #elif __APPLE__
-    pipeline = gst_parse_launch("udpsrc port=5000 ! application/x-rtp,media=(string)audio, clock-rate=(int)44100, width=16, height=16,"
-                                " encoding-name=(string)L16, encoding-params=(string)1, channels=(int)1, channel-positions=(int)1, payload=(int)96 "
-                                " ! rtpjitterbuffer mode=synced ! rtpL16depay ! audioconvert ! osxaudiosink sync=true", &gst_error);
+    pipeline = gst_parse_launch("udpsrc port=5000 ! application/x-rtp,media=(string)audio, clock-rate=(int)44100, width=16, height=16, \
+                                 encoding-name=(string)L16, encoding-params=(string)1, channels=(int)1, channel-positions=(int)1, payload=(int)96 \
+                                 ! rtpjitterbuffer mode=synced ! rtpL16depay ! audioconvert ! osxaudiosink sync=true", &gst_error);
 #elif
 #error "Platform undefined in gstreamerlistener.cpp"
 #endif
